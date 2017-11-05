e253ac47af59e234cf894705b2ee4d3ef416c644
beck <cabing2005@gmail.com>
2017-11-05 11:27:54 +0800

    go

--- src/test_pool.go
+++ src/test_pool.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strconv"
+	"sync/atomic"
 	"time"
 	"workpool"
 )
@@ -27,7 +29,60 @@ func (a *Arunner) Run(avar interface{}) (interface{}, error) {
 }
 
 func main() {
-	tpool2()
+	//tpool1()
+	//tpool2()
+	tpool3()
+}
+
+//创建一个connect
+var connectId int32 = 0
+
+type DbConnect struct {
+	cid int32
+}
+
+func (db *DbConnect) getConnectId() int32 {
+	return db.cid
+}
+
+func (db *DbConnect) Close() error {
+	return nil
+}
+
+//创建连接
+type DbConnectCreater struct{}
+
+func (dbc *DbConnectCreater) CreateConnect() (io.Closer, error) {
+
+	id := atomic.AddInt32(&connectId, 1)
+
+	return &DbConnect{cid: id}, nil
+}
+
+func tpool3() {
+	fmt.Println("test connect pool3")
+	creater := &DbConnectCreater{}
+	pool3, err := workpool.NewConnectPool(10, creater)
+	if err != nil {
+		log.Println("create faild")
+	}
+	defer pool3.Close()
+	//放入10个连接
+	for i := 0; i < 10; i++ {
+		conect, err := creater.CreateConnect()
+		if err == nil {
+			pool3.PutConnect(conect)
+		}
+	}
+
+	for i := 0; i < 12; i++ {
+		connect, err := pool3.GetConnect()
+		if err == nil {
+			log.Println("connect_id=", connect.(*DbConnect).getConnectId())
+
+		}
+	}
+
 }
 
 func tpool2() {
